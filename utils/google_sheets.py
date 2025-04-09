3c0662e49124a42327c8287ae275ff03366b9703
George <geousa@Georges-MacBook-Air.local>
2025-04-09 15:51:31 +0300

    Переход на переменную окружения вместо файла credentials.json

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -1,9 +1,14 @@
 import gspread
+import json
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 
 def save_feedback_to_google_sheets(name, user_id, message):
+    # Загружаем JSON-ключ из переменной окружения
+    credentials_json = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, scope)
     client = gspread.authorize(creds)
     sheet = client.open("ImpactRU Feedback").sheet1
     sheet.append_row([name, user_id, message])
+
