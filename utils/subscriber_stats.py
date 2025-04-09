de55947aeb16c8a1898cc7537546bcac1f9c225c
George <geousa@Georges-MacBook-Air.local>
2025-04-09 19:22:22 +0300

    Добавлен модуль для подсчета подписчиков

---
+++ utils/subscriber_stats.py
@@ -0,0 +1,16 @@
+import gspread
+import json
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+
+def get_subscriber_count():
+    credentials_json = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open("ImpactRU Subscribers").sheet1
+    records = sheet.get_all_records()
+    return len(records)
+
+
